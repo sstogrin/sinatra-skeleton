4772185debeb313178578f63a6940e0a18564dd6
Shayne Stogrin <sstogrin@outlook.com>
2015-05-12 13:25:20 -0600

    Initial Sinatra web pages and actions.

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,42 @@
 get '/' do
   erb :index
 end
+
+# get the login page
+get '/login' do
+  erb :login
+end
+
+# submit the login page
+post '/login' do
+  redirect '/'  # go to the home page for now
+end
+
+# get the logout page?
+get '/logout' do
+  erb :logout
+end
+
+put '/logout' do
+  redirect '/'  # go to the home page for now
+end
+
+# get the signup page
+get '/signup' do
+  erb :signup
+end
+
+# submit the signup page
+post '/signup' do
+  redirect '/' # go to the home page for now
+end
+
+# get the profile page
+get '/profile' do
+  erb :profile
+end
+
+# submit change from the profile page
+post '/profile' do
+  redirect '/' # go to home page for now
+end
